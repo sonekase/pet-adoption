e61e4dd7672078f3db4c3a8afb40dc9108806619
Anousone Kaseumsouk, Eliot Charette, Jean Jia, Lan Dam and Swati Sahay <anousone@>
2018-06-18 14:37:10 -0700

    Remove extra forms from index.

--- js/scripts.js
+++ js/scripts.js
@@ -16,13 +16,14 @@ var bear = new Animal ("Polly", "Polar bear","img/polarbear.jpg", "Available");
 var arrayAnimals = [tiger, crocodile, orca, otters, panda, bear];
 
 // Set the date we're counting down to
-var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
-
+// var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+var countDownDate = new Date().getTime() + 300000;
 // Update the count down every 1 second
 var x = setInterval(function() {
 
   // Get todays date and time
   var now = new Date().getTime();
+
 
   // Find the distance between now an the count down date
   var distance = countDownDate - now;
@@ -34,8 +35,10 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  // + minutes + "m " + seconds + "s ";
+
+  document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
